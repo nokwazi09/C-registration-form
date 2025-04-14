195944e350cbcc148db7aad8800cd265c9ef5ed6
nokwazi09 <149074670+nokwazi09@users.noreply.github.com>
2025-04-14 14:30:12 +0200

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Email] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Module name ] VARCHAR(50) NOT NULL, 
+    [Module Code ] SMALLINT NOT NULL, 
+    [Credits] SMALLINT NOT NULL, 
+    [start date] SMALLDATETIME NOT NULL
+)
